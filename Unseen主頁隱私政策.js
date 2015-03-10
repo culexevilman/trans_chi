24229bc8ba34ed553d1e9a4b3592bcbb432f0f46
culexevilman <jasonk@unseen.is>
2015-03-10 16:37:20 -0700

    updates

--- Unseen主頁隱私政策.js
+++ Unseen主頁隱私政策.js
@@ -1,14 +1,35 @@
-https://unseen.is/privacy.html
+### Privacy page
+label.privacy=隱私權
+privacy.unseen.policy=隱私權政策
+privacy.policy.0=Unseen，Ehf。 （“Unseen”，“我們”）提供了一個安全的信息服務，就是即時通訊和電子郵件，以及存儲的信息（以下簡稱“服務”）。
 
-隱私權政策
-
-
-Unseen，Ehf。 （“Unseen”，“我們”）提供了一個安全的信息服務，就是即時通訊和電子郵件，以及存儲的信息（以下簡稱“服務”）。
-
-Unseen全面致力於保護任何可能由Unseen收集的個人身份信息，從您使用本網站 atwww.unseen.is（以下簡稱“網站”）或本服務傳輸或存儲的保護所有通信的總隱私。Unseen保證，以最嚴格的隱私和保密政策的所有員工和Unseen的工作人員用結合到我們的伺服器以及數據明確責任的任何訪問了分層的方式。隱私政策也得到了相同的標準結合Unseen的分支機構。我們對於有關本公司員工違反Unseen的本隱私政策有著零容忍政策，是遠遠高於普通的行業標準。
+privacy.policy.0.=Unseen全面致力於保護任何可能由Unseen收集的個人身份信息，從您使用本網站 atwww.unseen.is（以下簡稱“網站”）或本服務傳輸或存儲的保護所有通信的總隱私。Unseen保證，以最嚴格的隱私和保密政策的所有員工和Unseen的工作人員用結合到我們的伺服器以及數據明確責任的任何訪問了分層的方式。隱私政策也得到了相同的標準結合Unseen的分支機構。我們對於有關本公司員工違反Unseen的本隱私政策有著零容忍政策，是遠遠高於普通的行業標準。
 
 Unseen也不會給第三方提供，共享，信息服務。但是，如果您選擇使用信用卡付款，你給任何資料上給出的信用卡處理器的支付網站。在這樣的情況下Unseen要求收件人，員工等將保護這些信息以本隱私政策一致的方式。因此第三方將於全權負責維護規定本隱私政策的標準。 - 為了確保您的個人信息的安全，您可以自由選擇比特幣或PayPal付款。
 
 在其他情況下，除非冰島送達至Unseen的法律程序，國家的法律要求之外，Unseen不會透露，交換，交易或出售您的任何數據 - 尤其是包括任何信息，提供有關您個人身份識別給任何第三方。任何第三方進行此類要求用戶則將立即被通知。
 
 所有識別您個人的信息，如電子郵件地址（ES），電話號碼（S）或您給Unseen提供的其他資料，為嚴格保密，並僅用於（一）提供服務，（二），為您提供有關服務的通知，如果您已經選擇接收他們，（三）如果Unseen授予這樣的權限，鏈接Unseen的用戶相互處理您的付款，以及（d）。Unseen的數據政策進一步明確有關數據加密和保留細節。
+
+### Privacy page
+label.privacy=隱私權
+privacy.unseen.policy=隱私權政策
+privacy.policy.0=Unseen公司（以下簡稱'Unseen'， '我們'， '我們的'， 或'我們'）提供安全的通信服務（以下簡稱'服務'）。 Unseen致力於保護您在使用本網站www.unseen.is （以下簡稱'網站'）時Unseen可能會收集到的任何個人標識信息。本通知設定了Unseen隱私權政策。訪問、使用本網站、在本網站註冊或使用本服務，表示您確認同意本隱私權政策。您亦應查看Unseen的服務條款，以了解更多管理您使用本網站和服務的政策信息。
+privacy.policy.1=1. 服務。 Unseen的服務將您的電子郵件信息用密鑰進行加密，而密鑰本身又根據您的密令進行加密。只有您自己知道您的密令。 Unseen服務傳輸並存儲您的電子郵件信息，但Unseen及其工作人員不能解密這些電子郵件信息，因為這需要您的密令，而我們沒有您的密令。根據法院或其它政府機構的執法要求，Unseen可能會被要求提供您加密過的電子郵件信息文件和您的加密密碼。然而，因為Unseen沒有您的密令，因而無法提供。
+privacy.policy.2.1=2. 信息收集。 Unseen本著誠信的原則，將盡可能少的收集您的必要信息以便為您提供服務。使用本服務要求您在註冊時向Unseen提供您的電子郵件地址，電話號碼和其它個人標識信息。當您付費購買升級服務時，Unseen也會收集並存儲個人標識信息。為支付該交易收集的個人標識信息可能包括您的姓名、地址、電話號碼、信用卡號碼和信用卡安全碼。
+privacy.policy.2.2=在您訪問網站和使用服務時，我們也可能會自動收集並存儲某些非個人標識信息。例如，當您訪問網站時我們可能會用'cookies'來獲取信息。這些信息可能會包括您的互聯網協議地址（IP）、瀏覽器類型、您訪問的日期和時間、您的硬件的類型和型號、您的設備的標識符、您的計算機的操作系統、網絡信息，以及您的互聯網服務供應商的域名。這些信息用於方便訪問和改進本網站和本服務。
+privacy.policy.2.3=Unseen， ehf.， Reg.id. 420413-0680， Idunnarbrunni 5， 113 Reykjavik， Iceland， is considered a controller in relation to the processing of these personal information collected.
+privacy.policy.3.1=3. 個人標識信息的使用。所有您所提供給Unseen的個人標識信息，如電子郵件地址、電話號碼或其它個人標識信息，都是屬於隱私信息並僅被用於(a) 提供本服務(b) 提供與本服務相關的通知(c) 處理您的付款(d) 驗證您的電子郵件地址或電話號碼(e) 在您授權許可下，將您和其他Unseen用戶連接。 Unseen不應以其它方式向任何第三方出售或提供您個人標識信息，除非依照法律規定或誠意相信在以下情況必須這樣做：(a) 符合法律規定或遵照送達Unseen的法律程序的要求，或(b) 保護和捍衛Unseen的權利或財產。如果Unseen與第三方如信用卡處理公司分享個人標識信息， Unseen將在切實可行的情況下，要求該信息接收者以符合本隱私權政策的方式保護該信息，但Unseen不保證第三方如何使用個人標識信息。
+privacy.policy.3.2=Unseen亦可能用收集到的非個人標識信息摘要或匯總信息來改進本網站和服務。若Unseen被第三方收購，您向Unseen提供的個人標識信息作為Unseen的一種資產，可能會被作為資產轉讓給第三方。
+privacy.policy.3.3=如果您選擇不再接受Unseen的電子郵件，可以隨時發郵件聯繫我們support@unseen.is 。
+privacy.policy.3.4=Unseen的政策是尊重所有用戶的隱私，但我們不能保證用戶的信息不被披露。
+privacy.policy.3.5=Unseen also may use non-personal identifying summary or aggregate information that Unseen collects， including to improve the Website and Service.
+privacy.policy.4=4. 電子郵件和密鑰。 Unseen能訪問並存儲您加密之後的郵件信息和加密過的密鑰。因為該加密是基於您的密令，而我們無法取得您的密令，所以Unseen無法解密您的電子郵件。
+privacy.policy.5=5. 與第三方網站的鏈接。我們向用戶提供連接到第三方網站的鏈接的服務。但對於這類第三方網站的信息收集和隱私保護措施可能不同於Unseen，因此我們無法控制亦概不負責。
+privacy.policy.6=6. 訪問和更正個人信息。按照網站上的指令，您可以在網站上訪問、更正、更新、和/或刪除您所提供給Unseen的任何個人標識信息，您亦可以退訂本服務。
+privacy.policy.7=7. 免責聲明。為了保護個人標識信息免於丟失、誤用、改動或破壞，Unseen採用了普遍接受的技術標準和操作安全性。 Unseen只讓被授權的員工得到個人標識信息的訪問方式，並要求這些員工對這類信息進行保密。儘管採取這些預防措施，Unseen不保證未經授權的人員沒有訪問您的個人標識信息的方式。
+privacy.policy.8=8. 兒童在線隱私保護。 Unseen理解保護兒童隱私權的重要性，尤其是在網絡在線的環境中。本網站不是為13歲或13歲以下的兒童設計的，Unseen不會特意收集或保存任何13歲以下兒童的信息。
+privacy.policy.9=9. 同意、修改。使用本網站，即表明您同意Unseen以本隱私權政策裡所述的方式收集、使用並存儲您的信息。您訪問本網站、在本網站註冊與使用本服務，以及任何您與Unseen關於隱私權的爭議都接受本隱私權政策及Unseen服務條款的製約。 Unseen保留自行決定、隨時改動、修改、添加或部分刪除本隱私權政策的權利。請時常檢查本頁的改動。您同意使用本網站和服務將受到當時最新版本的Unseen隱私權政策的約束。
+privacy.policy.10=10. 聯繫我們的隱私部門。如果您對於我們的隱私權政策有任何疑問，請聯繫support@unseen.is。
+privacy.policy.11=11. CONTACT OUR PRIVACY DEPARTMENT. If you have any questions about our Privacy Policy， please contact us at support@unseen.is.
+privacy.policy.date=2014年10月
